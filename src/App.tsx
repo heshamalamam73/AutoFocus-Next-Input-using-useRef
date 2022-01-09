995b21a4ac0f4c65f8c7d0480d20cefacf6b4926
Mahmoud Okily <36233253+heshamalamam73@users.noreply.github.com>
2022-01-09 17:53:17 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import "./styles.css";
+import Input from "./components/Input";
+import { useRef, useEffect } from "react";
+
+export default function App() {
+  const firstNameRef = useRef<HTMLElement | null>(null);
+  const lastNameRef = useRef<HTMLElement | null>(null);
+  const submitRef = useRef<HTMLElement | null>(null);
+
+  // if you want AutoFocus for the first input when component render
+
+  // useEffect(() => {
+  //   if (firstNameRef.current) {
+  //     firstNameRef.current.focus();
+  //   }
+  // }, []);
+  const onFirstNameKeyDown = (e: any) => {
+    if (e.key === "Enter" && lastNameRef.current) {
+      lastNameRef.current.focus();
+    }
+  };
+
+  const onLastNameKeyDown = (e: any) => {
+    if (e.key === "Enter" && submitRef.current) {
+      submitRef.current.focus();
+    }
+  };
+
+  const onSubmitKeyDown = (e: any) => {
+    if (e.key === "Enter" && submitRef.current) {
+      alert("submited");
+    }
+  };
+  return (
+    <div className="App">
+      <h1>React Form AutoFocus With UseRef </h1>
+      <Input
+        ref={firstNameRef}
+        type="text"
+        onKeyDown={onFirstNameKeyDown}
+        placeholder="firstName"
+      />
+      <Input
+        ref={lastNameRef}
+        type="text"
+        onKeyDown={onLastNameKeyDown}
+        placeholder="firstName"
+      />
+      <Input ref={submitRef} type="submit" onKeyDown={onSubmitKeyDown} />
+    </div>
+  );
+}
---
+++ src/components/Input.tsx
@@ -0,0 +1,26 @@
+import { forwardRef, LegacyRef } from "react";
+
+export type InputProps = {
+  placeholder?: string;
+  onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  type?: string;
+  onChange?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+};
+export type Ref = LegacyRef<HTMLInputElement> | undefined;
+
+const InputContainer = (
+  { placeholder, onKeyDown, type }: InputProps,
+  ref: Ref
+) => {
+  return (
+    <input
+      ref={ref}
+      placeholder={placeholder}
+      onKeyDown={onKeyDown}
+      type={type}
+    />
+  );
+};
+
+const Input = forwardRef(InputContainer);
+export default Input;
